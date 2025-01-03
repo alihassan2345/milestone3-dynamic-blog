389d4f3b3c95cb94a98873f6d297127b5c8cbea1
= <=>
2025-01-03 18:00:11 +0500

    add comment section

--- next-sanity-blog/app/(blog)/alert-banner.tsx
+++ next-sanity-blog/app/(blog)/alert-banner.tsx
@@ -1,4 +1,7 @@
+import Link from "next/link";
+
 // "use client";
+
 
 // import { useRouter } from "next/navigation";
 // import { useSyncExternalStore, useTransition } from "react";
@@ -21,11 +24,16 @@ export default function AlertBanner() {
 
   return (
     <div
-      className=" fixed top-0 left-0 z-50 w-full border-b bg-white/95 text-black backdrop-blur"
+      className=" fixed top-0 left-0 z-50 w-full border-b bg-white/95 text-black backdrop-blur flex items-center justify-between px-4 h-45"
     >
-      <div className="py-2 text-center text-sm">
-        Ai tecnology Blog
+      <div className="py-2 text-center text-md ">
+        <h1 className="font-bold">Ai tecnology Blog</h1>
       </div>
+      <nav className="flex gap-7 text-md font-bold m-4">
+        <Link href='/' className="hover:text-red-500 ">Home</Link>
+        <Link href='/about' className="hover:text-red-500 ">About</Link>
+        <Link href='/contact' className="hover:text-red-500 ">Contact</Link>
+      </nav>
     </div>
   );
 }
